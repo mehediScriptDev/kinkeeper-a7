ecf189df9aa4fe80a8ead71392db9eb1b068ffe1
mehedi <mehedi25122003@gmail.com>
2026-04-14 20:45:32 +0600

    details page added

--- src/app/components/Friends.jsx
+++ src/app/components/Friends.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React, { use } from "react";
 
 const Friends = ({ friends }) => {
@@ -19,19 +20,20 @@ const Friends = ({ friends }) => {
             color = "bg-[#244D3F]";
     }
         return (
-          <div key={friend.id} className="card flex flex-col justify-center items-center text-black shadow p-4 md:p-6 bg-white">
-            <Image
-            className="rounded-full"
-              src={friend.picture}
-              alt={friend.name}
-              width={98}
+          <Link href={`/${friend.id}`}>
+            <div key={friend.id} className="card flex flex-col justify-center items-center text-black shadow p-4 md:p-6 bg-white">
+              <Image
+                className="rounded-full"
+                src={friend.picture}
+                alt={friend.name}
+                width={98}
               height={98}
             />
             <h1 className="text-[#1F2937] font-bold text-xl py-3">{friend.name}</h1>
             <p className="text-xs text-[#64748B] mb-2">{friend.days_since_contact}d ago</p>
             <p className="text-[#244D3F] space-x-1 mb-2 text-xs">{friend.tags.map(tag=> <span className="bg-[#CBFADB] px-2 py-0.5 rounded-full" key={tag}>{tag}</span>)}</p>
             <p className={`text-xs px-2 py-0.5 rounded-full text-white ${color}`}>{friend.status}</p>
-          </div>
+          </div></Link>
         );
       })}
     </div>
